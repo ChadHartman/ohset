4b081ea4b4e67fb2bbf5585f3bdf7044801f97d8
Chad Hartman <cmhartman@gmail.com>
2025-10-07 15:51:36 -0700

    wip

--- test/main.c
+++ test/main.c
@@ -1,6 +1,14 @@
 #include <ohset.h>
 #include <stdlib.h> // EXIT_SUCCESS
+#include <test/testlist.h>
 
 int main() {
+
+  const size_t test_count = sizeof(tests) / sizeof(test_t);
+  for (size_t i = 0; i < test_count; ++i) {
+    printf("| %s |\n", tests[i].name);
+    tests[i].func();
+  }
+
   return EXIT_SUCCESS;
 }
---
+++ test/test/testlist.h
@@ -0,0 +1,19 @@
+#ifndef TEST_LIST_H
+#define TEST_LIST_H
+
+#include <test/test.h>
+
+#define TEST_REG(test_name) {.name = #test_name, .func = test_##test_name}
+
+typedef struct test_t {
+  const char *name;
+  void (*func)();
+} test_t;
+
+TEST(hash);
+
+const test_t tests[] = {
+    TEST_REG(hash),
+};
+
+#endif

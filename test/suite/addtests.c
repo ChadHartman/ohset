23b8c92fb7d258e14a895aea76b4048776503131
Chad Hartman <cmhartman@gmail.com>
2025-10-07 18:38:43 -0700

    wip

--- test/suite/addtests.c
+++ test/suite/addtests.c
@@ -1,7 +1,8 @@
 #include <ohset.h>
+#include <test/allocator.h>
 #include <test/test.h>
 
-TEST(add) {
+static void test_add_many() {
 
   ASSERT_FALSE(ohset_add(NULL, NULL));
 
@@ -18,4 +19,27 @@ TEST(add) {
   }
 
   ohset_free(set);
+}
+
+static void test_add_alloc_failed_2nd_time() {
+
+  const size_t item = 42;
+  allocator_t allocator = {
+      .max_times = 1,
+  };
+  ohset_t *restrict set = ohset_new(&(ohset_config_t){
+      .alloc = alloc,
+      .alloc_ctx = &allocator,
+      .item_size = sizeof(size_t),
+  });
+
+  ASSERT_NON_NULL(set);
+  ASSERT_FALSE(ohset_add(set, &item));
+
+  ohset_free(set);
+}
+
+TEST(add) {
+  test_add_many();
+  test_add_alloc_failed_2nd_time();
 }
